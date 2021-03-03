d24c3c22b6d7996bc42f98f4a4aea222c6cf69fe
Jean-Philipp <jean-philipp@web.de>
2021-03-03 16:07:18 +0100

    worked on the data analysis

--- thesisDataExport.sh
+++ thesisDataExport.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection classrooms --type JSON --out classrooms.json --jsonArray
+mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection classrooms --type JSON --out classrooms.json --jsonArray
 #mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection companies --type JSON --out companies.json --jsonArray
 #mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection counter --type JSON --out counter.json --jsonArray
-#mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection globalSurveys --type JSON --out globalSurveys.json --jsonArray
+mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection globalSurveys --type JSON --out globalSurveys.json --jsonArray
 mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection pupils --type JSON --out pupils.json --jsonArray
 mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection statistics --type JSON --out statistics.json --jsonArray
 #mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection taskSeries --type JSON --out taskSeries.json --jsonArray
