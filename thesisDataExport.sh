ae2efa404044e5f3efddce1af24b74dc5d215ebc
Jean-Philipp <jean-philipp@web.de>
2021-03-11 17:21:50 +0100

    finished preprocessing and put everything into a dataframe

--- thesisDataExport.sh
+++ thesisDataExport.sh
@@ -5,7 +5,7 @@ mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pea
 mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection globalSurveys --type JSON --out globalSurveys.json --jsonArray
 mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection pupils --type JSON --out pupils.json --jsonArray
 mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection statistics --type JSON --out statistics.json --jsonArray
-#mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection taskSeries --type JSON --out taskSeries.json --jsonArray
+mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection taskSeries --type JSON --out taskSeries.json --jsonArray
 #mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection teachers --type JSON --out teachers.json --jsonArray
 #mongoexport --uri mongodb+srv://jp:vPTdaSH22us6L6Oo@pearup.r81v8.mongodb.net/pearup --collection users --type JSON --out users.json --jsonArray
 echo "console log"
